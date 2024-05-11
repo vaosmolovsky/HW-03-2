ddd5461b94127b6b29fcffc42e97a8e7d137d690
Vladislav <Vladislav@FENIX-PC>
2024-05-11 13:26:35 +0300

    dev

--- HW-03-2/Program.cs
+++ HW-03-2/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace HW_03_2
+﻿using System;
+
+
+namespace HW2
 {
-    internal class Program
+    public class SpaceOnDisk
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите путь к папке:");
+            string filePath = Console.ReadLine();
+            double answer = Space(0, filePath);
+            Console.WriteLine(answer);
+        }
+        public static double Space(double size, string filePath)
+        {
+            double size1 = size;
+            var di = new DirectoryInfo(filePath);
+            try
+            {
+                if (di.Exists)
+                {
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        size1 += file.Length;
+                    }
+                    foreach (DirectoryInfo dir in di.GetDirectories())
+                    {
+                        string path = dir.FullName;
+                        size1 += Space(size, path);
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return size1;
         }
     }
 }
